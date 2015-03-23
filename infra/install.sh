e51bca9e6abc623dd3feb6ed25f43aa50b59e7c3
Saltuk Alakus <saltukalakus@gmail.com>
2015-03-23 18:19:20 +0200

    install script updated

--- infra/install.sh
+++ infra/install.sh
@@ -1,8 +1,45 @@
 #!/bin/bash
-# TODO: force run as root
+
+# Force run as root
+if [ $(id -u) != "0" ]
+    then
+        sudo "$0" "$@"  # Modified as suggested below.
+        exit $?
+fi
+
+# Installations
+# TODO: Add mongodb
+apt-get install nodejs
+apt-get install npm
 apt-get install nginx
 apt-add-repository ppa:vbernat/haproxy-1.5
 apt-get update
 apt-get install haproxy
+
+# Haproxy conf setup
 mv /etc/init.d/haproxy ~ #  Haproxy is controlled by upstart
+cp ./haproxy/haproxy.cfg /etc/haproxy
+
+# Nginx conf setup
+cp -v ./nginx/* /etc/nginx/sites-available
+ln -s /etc/nginx/sites-enabled/nginx-conf /etc/nginx/sites-available/nginx-conf
+ln -s /etc/nginx/sites-enabled/nginx-conf2 /etc/nginx/sites-available/nginx-conf2
 mv /etc/init.d/nginx ~ #  Nginx is controlled by upstart
+
+# Copy upstart files
+cp -v ./upstart/* /etc/init
+
+# SSL keys
+pushd .
+cd ./ssl/
+./ssl-key-gen.sh
+popd
+
+# Restart all
+stop xuser
+stop nginx
+stop haproxy
+start xuser
+start nginx
+start haproxy
+
--- infra/ssl/ssl-key-gen.sh
+++ infra/ssl/ssl-key-gen.sh
@@ -2,4 +2,4 @@
 
 openssl req -x509 -nodes -newkey rsa:2048 -keyout site.key -out site.crt -days 10000
 cat site.crt site.key > site.pem
-cp site.pem /etc/ssl/private/
+cp -v site.pem /etc/ssl/private/
