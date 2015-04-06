4bd4d5e8a78b5637144194302cddd9d1c51141db
Saltuk Alakus <saltukalakus@gmail.com>
2015-04-06 17:28:18 +0300

    upstart scripts for mongodb cont.

--- infra/mongodb/init.sh
+++ infra/mongodb/init.sh
@@ -7,8 +7,25 @@ if [ $(id -u) != "0" ]
         exit $?
 fi
 
+# Kill all running mongod processes
+pkill -9 mongod
+
+rm -Rf /data-mongodb
+mkdir -p /data-mongodb/
+
+mkdir -p /data-mongodb/xuser-1
+cp -v primary.conf /data-mongodb/xuser-1
+
+mkdir -p /data-mongodb/xuser-2
+cp -v secondary.conf /data-mongodb/xuser-2
+
+mkdir -p /data-mongodb/backup
+cp -v backup.conf /data-mongodb/backup
+
+chown mongodb:mongodb -Rf /data-mongodb
+
 # First time configuration script, this needs to be executed once.
-su -u mongodb mongod -f ./primary.conf
-su -u mongodb mongod -f ./secondary.conf
-su -u mongodb mongod -f ./backup.conf
-su -u mongodb mongo 127.0.0.1:27001/admin init.js
+mongod -f ./primary.conf
+mongod -f ./secondary.conf
+mongod -f ./backup.conf
+mongo 127.0.0.1:27001/admin init.js
