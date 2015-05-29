65398165010eff35582b96c9f4f640f3a4c1c476
Saltuk Alakus <saltukalakus@gmail.com>
2015-05-29 16:50:09 +0300

    minor changes on versions and install script

--- infra/single/install.sh
+++ infra/single/install.sh
@@ -15,7 +15,7 @@ apt-get -y install python-pip
 pip install docopt
 
 # NodeJs
-apt-get update
+apt-get -y update
 apt-get -y install nodejs
 apt-get -y install npm
 npm install gulp -g
@@ -37,7 +37,7 @@ echo $PROJECT_PATH
 apt-get -y install nginx
 # Haproxy
 apt-add-repository -y ppa:vbernat/haproxy-1.5
-apt-get update
+apt-get -y update
 apt-get -y --force-yes install haproxy
 # MongoDB
 # Fix Failed global initialization: BadValue Invalid or no user locale set.
@@ -49,12 +49,12 @@ export LC_ALL=en_US.UTF-8
 locale-gen en_US.UTF-8
 dpkg-reconfigure locales
 # Remove the old mongo
-apt-get remove mongodb* --purge
-apt-get autoremove
+apt-get -y remove mongodb* --purge
+apt-get -y autoremove
 # Install mongo
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
-apt-get update
+apt-get -y update
 apt-get -y install mongodb-org
 # Fix mongo version
 echo "mongodb-org hold" | sudo dpkg --set-selections
@@ -84,12 +84,12 @@ cd ./mongodb
 popd
 # Redis
 add-apt-repository -y ppa:rwky/redis
-apt-get update
-apt-get install -y redis-server
+apt-get -y update
+apt-get -y install redis-server
 
 # Haproxy conf setup
 /etc/init.d/haproxy stop
-mv /etc/init.d/haproxy ~ #  Haproxy is controlled by upstart
+mv -fv /etc/init.d/haproxy ~ #  Haproxy is controlled by upstart
 cp -fv ./haproxy/haproxy.cfg /etc/haproxy
 
 # Nginx conf setup
@@ -106,7 +106,7 @@ python ../helpers/auto_replace.py --file=/etc/nginx/sites-available/nginx-node2 
                                   --search="#AUTO_REPLACE_PR_PATH" \
                                   --replace=$PROJECT_PATH
 
-mv /etc/init.d/nginx ~ #  Nginx is controlled by upstart
+mv -fv /etc/init.d/nginx ~ #  Nginx is controlled by upstart
 
 # Redis conf setup
 mkdir -p /var/log/redis
@@ -114,7 +114,7 @@ cp -fv ./redis/*.conf /etc/redis
 chown redis:redis /etc/redis/*.conf
 
 # Copy upstart files
-cp -v ./upstart/* /etc/init
+cp -fv ./upstart/* /etc/init
 python ../helpers/auto_replace.py --file=/etc/init/nodejs-instance.conf \
                                   --search="#AUTO_REPLACE_COOKIE_SECRET" \
                                   --replace="42rerwejfkj9434cds5ewejd"
