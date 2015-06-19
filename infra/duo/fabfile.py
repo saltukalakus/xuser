042b73c3369e40f28d6c0a62553da87fe5c8b4e5
Saltuk Alakus <saltukalakus@gmail.com>
2015-06-19 17:46:20 +0300

    Update the fabfile

--- infra/duo/fabfile.py
+++ infra/duo/fabfile.py
@@ -3,18 +3,23 @@ from fabric.api import *
 env.hosts = ['52.28.149.188']
 env.user = 'ubuntu'
 env.key_filename = '/home/keys/key.pem'
- 
+
+@with_settings(warn_only=True)
 def remote_git_checkout():
-    env.warn_only = True
     run('cd /home/ubuntu')
     run('git clone https://github.com/saltukalakus/xuser')
 
+@with_settings(warn_only=True)
 def remote_git_pull():
-    env.warn_only = True
-    run('cd /home/ubuntu/xuser')
-    run('git pull')
+    with cd('/home/ubuntu/xuser'):
+        run('pwd')
+        run('git pull')
+    run('pwd')
 
+@with_settings(warn_only=True)
 def remote_install():
-    env.warn_only = True
-    run('cd /home/ubuntu/xuser/infra/duo')
-    run ('./install_master.sh')
+    with cd('/home/ubuntu/xuser/infra/duo'):
+        run('sudo su')
+        run('whoami')
+        run ('./install_master.sh')
+
