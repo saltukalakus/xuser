9264336cb958011bfdd0eec563289dd6912d2a3e
Saltuk Alakus <saltukalakus@gmail.com>
2015-06-19 10:56:26 +0300

    Update on fabfile

--- infra/duo/fabfile.py
+++ infra/duo/fabfile.py
@@ -4,11 +4,17 @@ env.hosts = ['52.28.149.188']
 env.user = 'ubuntu'
 env.key_filename = '/home/keys/key.pem'
  
-def local_uname():
-    local('uname -a')
- 
-def remote_uname():
-    run('uname -a')
+def remote_git_checkout():
+    env.warn_only = True
+    run('cd /home/ubuntu')
+    run('git clone https://github.com/saltukalakus/xuser')
 
-def taskA():
-    run('ls')
+def remote_git_pull():
+    env.warn_only = True
+    run('cd /home/ubuntu/xuser')
+    run('git pull')
+
+def remote_install():
+    env.warn_only = True
+    run('cd /home/ubuntu/xuser/infra/duo')
+    run ('./install_master.sh')
