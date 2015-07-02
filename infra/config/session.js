3c5706639087477f46e824f98fda9006f8c1305b
Saltuk Alakus <saltukalakus@gmail.com>
2015-07-02 14:41:48 +0300

    minor change on session conf file

--- infra/config/session.js
+++ infra/config/session.js
@@ -4,8 +4,8 @@ module.exports = {
                  'host2': '#AUTO_REPLACE_HOST2',
                  'host3': '#AUTO_REPLACE_HOST3',
                  'port1': '#AUTO_REPLACE_PORT1',
-                 'port2': '#AUTO_REPLACE_PORT1',
-                 'port3': '#AUTO_REPLACE_PORT1'
+                 'port2': '#AUTO_REPLACE_PORT2',
+                 'port3': '#AUTO_REPLACE_PORT3'
                  },
     'cluster': {'name': '#AUTO_REPLACE_CLUSTER_NAME' }
 };
